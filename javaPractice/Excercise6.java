e64b2b67c6882cb509ffc11234f041e896336bf2
Will B <tryarc@comcast.net>
2022-09-13 14:02:51 -0400

    first pushed exercise

---
+++ javaPractice/Excercise6.java
@@ -0,0 +1,30 @@
+package javaPractice;
+import java.util.Scanner;
+
+public class Excercise6 {
+	 public static void main(String[] args) {
+		  Scanner in = new Scanner(System.in);
+		   
+		  System.out.print("Input first number: ");
+		  int a = in.nextInt();
+		   
+		  System.out.print("Input second number: ");
+		  int b = in.nextInt();
+		   
+		 
+		  System.out.println(a + " + " + b + " = " + 
+		  (a + b));
+		   
+		  System.out.println(a + " - " + b + " = " + 
+		  (a - b));
+		   
+		  System.out.println(a + " x " + b + " = " + 
+		  (a * b));
+		   
+		  System.out.println(a + " / " + b + " = " + 
+		  (a / b));
+		 
+		  System.out.println(a + " mod " + b + " = " + 
+		  (a % b));
+		 }
+}
